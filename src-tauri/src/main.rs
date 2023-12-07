54e8189e211fcbf732b0872217ea3c49955d502f
Guoqiang0839 <117185820+Guoqiang0839@users.noreply.github.com>
2023-12-08 00:34:16 +0800

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,19 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-  tauri::Builder::default()
-    .plugin(tauri_plugin_window_state::Builder::default().build())
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+use tauri::Manager;
+
+#[tauri::command]
+fn generate_note(chat_history: String) -> String {
+    // 这里应该是生成笔记的逻辑
+    // 现在我们只是简单地返回聊天历史
+    format!("生成的笔记: {}", chat_history)
 }
+
+fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![generate_note])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
